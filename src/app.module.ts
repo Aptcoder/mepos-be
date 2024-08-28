b7bdde681ec31c231be6e4a4ef8bb052f5c5b87d
leksyking <foogundipe@student.oauife.edu.ng>
2024-08-28 16:36:37 -0700

    first deployment on this branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,27 @@
 import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { StoreModule } from './store/store.module';
 import { RoleModule } from './role/role.module';
 import { ProductModule } from './product/product.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forRoot('mongodb://localhost:27018/mpos'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MEPOS_DB),
     StoreModule,
     RoleModule,
     ProductModule,
     TransactionModule,
   ],
+  providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
